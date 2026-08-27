2ce0b75baedbadb3fb0e81215bed4ecf1998aef3
LOFG-preston-dut <legendoffirstgeneration081007@gmail.com>
2026-08-27 21:55:37 +0700

    27-8-2026

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -8,18 +8,21 @@ void PrintArray(int arr[], int size){
     cout << endl;
 }
 
-void SelectionSort(int arr[], int size){
-    for (int i = 0; i < size; i++){
-        int MinIndex = i;
-        for (int j = i+1; j < size; j++){
-            if (arr[j] < arr[MinIndex]){
-                MinIndex = j;
+void BubbleSort(int arr[], int size){
+    for (int i = 0; i < size - 1; i++){
+        bool swapped = false;
+
+        for (int j = 0; j < size - i - 1; j++){
+            if (arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
             }
         }
-        if (MinIndex != i){
-            int temp = arr[i];
-            arr[i] = arr[MinIndex];
-            arr[MinIndex] = temp;
+
+        if (!swapped){
+            break;
         }
     }
 }
@@ -40,7 +43,7 @@ int main(){
     }
     cout << "\n The original array: ";
     PrintArray(arr,size);
-    SelectionSort(arr,size);
+    BubbleSort(arr,size);
     cout <<endl;
     cout << "\n The sorted array: ";
     PrintArray(arr,size);
