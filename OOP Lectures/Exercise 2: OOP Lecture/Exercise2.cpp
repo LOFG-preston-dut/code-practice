f3d277d20d9df067d702077b31e844e4e90fc212
LOFG-preston-dut <legendoffirstgeneration081007@gmail.com>
2026-08-29 23:53:59 +0700

    28-8-2026

--- OOP Lectures/Exercise 2: OOP Lecture/Exercise2.cpp
+++ OOP Lectures/Exercise 2: OOP Lecture/Exercise2.cpp
@@ -1,127 +1,81 @@
 #include <iostream>
-
 using namespace std;
 
-struct Student {
-    char fullName[50];
-    float literatureScore;
-    float mathScore;
-    float averageScore;
+struct HocSinh{
+    char HoTen[100];
+    double DiemToan;
+    double DiemVan;
 };
 
-struct Node {
-    Student data;
+struct Node{
+    HocSinh data;
     Node* next;
 };
 
-struct LinkedList {
-    Node* head;
+struct LinkedList{
+    Node* head = nullptr;
 };
 
-void initList(LinkedList& list) {
-    list.head = NULL;
+double AverageScore(HocSinh hs){
+    return (hs.DiemToan + hs.DiemVan) / 2.0;
 }
 
-Node* createNode(Student student) {
-    Node* p = new Node;
-    p->data = student;
-    p->next = NULL;
-    return p;
+const char* Classification(double avg){
+    if (avg >= 8.0) return "Gioi";
+    else if (avg >= 7.0 && avg < 8.0) return "Kha";
+    else if (avg >= 5.0 && avg < 7.0) return "Trung Binh";
+    else return "Yeu";
 }
 
-float calculateAverage(float literature, float math) {
-    return (literature + math) / 2.0;
+void InputStudent(HocSinh &hs){
+    cout << "Nhap ho ten hoc sinh: ";
+    cin.ignore();
+    cin.getline(hs.HoTen, 100);
+    cout << "Nhap diem Toan: ";
+    cin >> hs.DiemToan;
+    cout << "Nhap diem Van: ";
+    cin >> hs.DiemVan;
 }
 
-void printGrade(float average) {
-    if (average >= 8.0) {
-        cout << "Gioi";
-    } else if (average >= 7.0) {
-        cout << "Kha";
-    } else if (average >= 5.0) {
-        cout << "Trung binh";
-    } else {
-        cout << "Yeu";
+void OutputStudentList(LinkedList &list){
+    cout << "Danh sach hoc sinh: \n";
+    Node* temp = list.head;
+    while (temp != nullptr){
+        float average = AverageScore(temp->data);
+        cout << "Ho ten: " << temp->data.HoTen << ", Diem Toan: " << temp->data.DiemToan
+             << ", Diem Van: " << temp->data.DiemVan << ", Diem trung binh: " << average
+             << ", Xep loai: " << Classification(average) << endl;
+             temp = temp->next;
     }
 }
 
-void inputStudent(Student& student) {
-    cin.ignore();
-    cout << "Nhap ho ten: ";
-    cin.getline(student.fullName, 50);
-    cout << "Nhap diem Van: ";
-    cin >> student.literatureScore;
-    cout << "Nhap diem Toan: ";
-    cin >> student.mathScore;
-    student.averageScore = calculateAverage(student.literatureScore, student.mathScore);
-}
-
-void appendNode(LinkedList& list, Student student) {
-    Node* p = createNode(student);
-    if (list.head == NULL) {
-        list.head = p;
+void AddStudent(LinkedList &list, HocSinh hs){
+    Node* newNode = new Node;
+    newNode->data = hs;
+    newNode->next = nullptr;
+    if (list.head == nullptr){
+        list.head = newNode;
     } else {
         Node* temp = list.head;
-        while (temp->next != NULL) {
+        while (temp->next != nullptr){
             temp = temp->next;
         }
-        temp->next = p;
+        temp->next = newNode;
     }
 }
 
-void inputStudentList(LinkedList& list, int count) {
-    for (int i = 0; i < count; i++) {
-        cout << "\n--- Nhap hoc sinh thu " << i + 1 << " ---\n";
-        Student student;
-        inputStudent(student);
-        appendNode(list, student);
-    }
-}
-
-void printStudent(Student student) {
-    cout << "Ho ten: " << student.fullName << endl;
-    cout << "Diem Van: " << student.literatureScore << endl;
-    cout << "Diem Toan: " << student.mathScore << endl;
-    cout << "Diem Trung Binh: " << student.averageScore << endl;
-    cout << "Xep loai: ";
-    printGrade(student.averageScore);
-    cout << endl;
-}
-
-void printStudentList(LinkedList list) {
-    cout << "\n=== DANH SACH HOC SINH ===\n";
-    Node* temp = list.head;
-    int index = 1;
-    while (temp != NULL) {
-        cout << "\nHoc sinh " << index++ << ":\n";
-        printStudent(temp->data);
-        temp = temp->next;
-    }
-}
-
-void freeList(LinkedList& list) {
-    Node* temp = list.head;
-    while (temp != NULL) {
-        Node* p = temp;
-        temp = temp->next;
-        delete p;
-    }
-    list.head = NULL;
-}
-
-int main() {
-    LinkedList list;
-    initList(list);
-
-    int count;
+int main(){
+    Node* head = nullptr;
+    int n;
     cout << "Nhap so luong hoc sinh: ";
-    cin >> count;
-
-    if (count > 0) {
-        inputStudentList(list, count);
-        printStudentList(list);
+    cin >> n;
+    LinkedList list;
+    for (int i = 0; i < n; ++i){
+        HocSinh hs;
+        InputStudent(hs);
+        AddStudent(list, hs);
     }
+    OutputStudentList(list);
 
-    freeList(list);
     return 0;
 }
